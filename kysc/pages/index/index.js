a89b6ce576dc504f8358d129d515054630fb2136
张雨 <zhangyu528@163.com>
2019-09-10 15:31:55 +0800

    [add]添加我的页面

--- kysc/pages/index/index.js
+++ kysc/pages/index/index.js
@@ -4,16 +4,15 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
   bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    // wx.navigateTo({
+    //   url: '../logs/logs'
+    // })
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
